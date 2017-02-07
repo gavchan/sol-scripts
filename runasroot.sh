df286da2fcee758fcfc2d064edda95895304eb72
Gavin <gavchan@users.noreply.github.com>
2017-02-06 22:12:44 -0600

    add root configuration files

---
+++ runasroot.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# Simple bash setup script to configuration files into root
+
+echo Copying configuration files to /root
+cp vimrc /root/.vimrc
+cp rbashrc /root/.bashrc
+cp bash_profile /root/.bash_profile
