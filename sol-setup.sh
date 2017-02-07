0b4ade524912776a4851aa04b58d8094eda2dec7
Gavin <gavchan@users.noreply.github.com>
2017-02-06 20:39:58 -0600

    Modify setup script

--- sol-setup.sh
+++ sol-setup.sh
@@ -2,10 +2,15 @@
 
 # Basic setup for Solaris 11.3
 
+echo Moving vimrc, bashrc, bash_profile to home folder
 cp vimrc.vim ~/.vimrc
 cp bashrc ~/.bashrc
+cp bash_profile ~/.bash_profile
+
+echo Installing Lynx and Samba
 pfexec pkg install lynx
-pfexec pkg install samba
+pfexec pkg install --accept samba
+
+echo Done.
 
 date
-echo Lynx and Samba installed.
