bbbfdddf5a3b8ac4a5b4c982292ec28f10721544
Gavin <gavchan@users.noreply.github.com>
2017-02-06 22:14:36 -0600

    minor fix

--- runasroot.sh
+++ runasroot.sh
@@ -2,6 +2,6 @@
 # Simple bash setup script to configuration files into root
 
 echo Copying configuration files to /root
-cp vimrc /root/.vimrc
+cp vimrc.vim /root/.vimrc
 cp rbashrc /root/.bashrc
 cp bash_profile /root/.bash_profile
